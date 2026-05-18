a520a0e59e3b6a8b0ae99d13a5dbcd3a4bc8efd0
BarisRz <barisg@pm.me>
2026-05-18 02:18:09 +0200

    feat: ajouter la navigation dans les composants ColorTitleSection2 et Error

--- src/components/ColorTitleSection2.jsx
+++ src/components/ColorTitleSection2.jsx
@@ -1,5 +1,8 @@
 import { Palette, Pipette } from "lucide-react";
+import { useNavigate, useLocation } from "react-router-dom";
 function ColorTitleSection2({ lampState }) {
+  const navigate = useNavigate();
+  const location = useLocation();
   return (
     <section
       className={
@@ -10,8 +13,17 @@ function ColorTitleSection2({ lampState }) {
       <div className="flex gap-2 items-center">
         <Palette className="w-4 h-4" /> Couleur
       </div>
-      <div className="flex items-center gap-2 rounded-3xl">
-        <Pipette className="w-4 h-4" /> Custom
+      <div
+        className="flex items-center gap-2 rounded-3xl"
+        onClick={() => {
+          if (location.pathname === "/") {
+            navigate("/color-wheel");
+          } else {
+            navigate("/");
+          }
+        }}
+      >
+        <Pipette className="w-4 h-4" />
       </div>
     </section>
   );
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,8 @@
 import { Bug } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export default function Error() {
+  const navigate = useNavigate();
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen-minus-40 gap-4">
@@ -14,6 +16,12 @@ export default function Error() {
           Faites <span className="font-bold text-white">F5</span> pour recharger
           la page.
         </p>
+        <button
+          className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-colors"
+          onClick={() => navigate("/")}
+        >
+          Retour à l'accueil
+        </button>
       </div>
     </>
   );
